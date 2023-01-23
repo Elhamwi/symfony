7a2cba5736ff94730b437a780da0a2c62097f811
Syrfa <aimane59800@gmail.com>
2023-01-23 16:33:46 +0100

    mise en place des methodes et des requêtes POST/GET

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,27 +4,30 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ContactController
+class ContactController extends AbstractController
 {
-    #[Route('/contact', methods: ['GET', 'HEAD'])]
+    #[Route('/contact', methods: ['GET'])]
     public function contact(): Response
     {
-    
-
         return new Response(
-            '<html><body><h1>fjjfeio</h1></body></html>'
+
+            '<html><body>GET contact</body></html>'
         );
+
+       
     }
 
-    #[Route('/contact', methods: ['POST', 'HEAD'])]
+    #[Route('/contact', methods: ['POST'])]
     public function contracted(): Response
     {
     
-
         return new Response(
-            '<html><body><h1>fjjfeio</h1></body></html>'
+
+            '<html><body>POST contracted</body></html>'
         );
+       
     }
 }
 ?>
